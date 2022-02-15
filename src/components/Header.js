305c15309d3db86b9b8d4e4574a56ced0320a856
originlimit <andres-log@protonmail.com>
2022-02-15 14:05:30 -0800

    styled header bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search'
+import LanguageIcon from '@mui/icons-material/Language'
+import ExpandMore from '@mui/icons-material/ExpandMore'
+import { Avatar } from '@mui/material'
 
 const Header = () => {
   return (
     <div className='header'>
         <img className='header__icon' src='https://dwglogo.com/wp-content/uploads/2017/09/React_logo.png' alt='header logo' />
         <div className='header__center'>
-            <div>center</div>
+            <input type='text'/>
+            <SearchIcon />
         </div>
-        <div>right</div>
+        <div className='header__right'>
+            <p>Be a host!</p>
+            <LanguageIcon />
+            <ExpandMore />
+            <Avatar />
+        </div>
     </div>
   )
 }
