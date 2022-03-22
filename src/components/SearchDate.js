24d906513e78b5745afd785702c071d48c6bd449
originlimit <andres-log@protonmail.com>
2022-03-21 22:43:24 -0700

    added the actual picker, search button, and guest input

--- src/components/SearchDate.js
+++ src/components/SearchDate.js
@@ -2,16 +2,34 @@ import React from 'react'
 import { useState } from 'react'
 import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css'
+import './SearchDate.css'
 import { DateRangePicker } from 'react-date-range'
+import { Button } from '@mui/material'
+import PeopleIcon from '@mui/icons-material/People'
 
 const SearchDate = () => {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
-  return (
-    <>
-        this is a test
-    </>
-  )
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection",
+    }
+
+    function handleSelect(ranges){
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+    return (
+        <div>
+            <DateRangePicker className='search' ranges={[selectionRange]} onChange={handleSelect} />
+            <h2>Number of guests <PeopleIcon /></h2>
+            <input min={0} defaultValue={2} type='number' />
+            <Button>Search CloneBNB</Button>
+        </div>
+    )
 }
 
 export default SearchDate
