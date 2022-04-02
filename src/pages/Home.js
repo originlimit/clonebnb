fa4b4058d915943be5f17b02e3d0798eb20b06c6
originlimit <andres-log@protonmail.com>
2022-04-02 15:56:04 -0700

    aligned text and added extra classes for better control

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
       <Hero />
       <div className='home__section'>
         <Card src='https://images.unsplash.com/photo-1559336305-613022155766?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' title='Online Experiences' desc='Unique activities and experiences we can do together, led by a world of hosts.'/>
-        <Card src='https://images.unsplash.com/photo-1622372738946-62e02505feb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=832&q=80' title='Unique Stays' desc='Spaces that are more than just a place to sleep.'/>
+        <Card src='https://images.unsplash.com/photo-1622372738946-62e02505feb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=832&q=80' title='Unique Stays' desc='Spaces that are more than just a place to sleep. Destinations that you are guaranteed to love'/>
         <Card src='https://images.unsplash.com/photo-1633602108759-40f528b59111?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' title='Entire Homes' desc='Comfortable, private places to share and make memories with those you value most.'/>
       </div>
       <div className='home__section'>
