eec9027feee3638ce14c502f94e9760d20ebf2eb
originlimit <andres-log@protonmail.com>
2022-04-03 12:33:13 -0700

    test for card spacing

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = ({src, title, desc, price}) => {
             <img className='card__img' src={src} alt='card visual' />
             <div className='card__info'>
                 <h2 className='card__content'>{title}</h2>
-                <h3 className='card__content'>{desc}</h3>
+                <h4 className='card__content'>{desc}</h4>
                 <h4 className='card__content card__price'>{price}</h4>
             </div>
         </div>
