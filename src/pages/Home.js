76a15296127c944ecb02cc622b548de15f0b61f9
originlimit <andres-log@protonmail.com>
2022-04-02 19:50:51 -0700

    footer component test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from '../components/Card'
 import DatePicker from '../components/DatePicker'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import './Home.css'
@@ -25,6 +26,7 @@ const Home = () => {
           <Card src='https://images.unsplash.com/photo-1644135151632-05e0611d473d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=938&q=80' title='Luxury Apartment' desc='Live like a king and lounge in this fully furnished luxury apartment.' price='$190 per night' />
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
