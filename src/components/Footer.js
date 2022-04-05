ab825b8e2dfe4c47b739a799c68dabff35bc8453
originlimit <andres-log@protonmail.com>
2022-04-04 19:01:04 -0700

    modified app structure to always show header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
   return (
     <div className='footer'>
         <div className='footer__container'>
-            <a className='footer__item'>Policies</a>
-            <a className='footer__item'>Cookies</a>
-            <a className='footer__item'>Events</a>
-            <a className='footer__item'>Contact</a>
+            <a href="#" className='footer__item'>Policies</a>
+            <a href="#" className='footer__item'>Cookies</a>
+            <a href="#" className='footer__item'>Events</a>
+            <a href="#" className='footer__item'>Contact</a>
         </div>
         <section id='copyright'>AirBNB Clone 2022 - No Copyright.</section>
     </div>
