3564415f02e3a6b46f1615df7c3eb680092c89e3
originlimit <andres-log@protonmail.com>
2022-04-22 01:00:04 -0700

    added content -- search page info

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,20 @@
+import { Button } from '@mui/material'
 import React from 'react'
 import './SearchPage.css'
 
 const SearchPage = () => {
   return (
-    <div>This is the SearchPage</div>
+    <div className='searchPage'>
+        <div className='searchPage__info'>
+            <p>60 stays | august 10 to august 23 | 2 guest</p>
+            <h2>Stays Nearby</h2>
+            <Button variant='outlined'>Cancellation Flexibility</Button>
+            <Button variant='outlined'>Type of Place</Button>
+            <Button variant='outlined'>Price</Button>
+            <Button variant='outlined'>Rooms and Beds</Button>
+            <Button variant='outlined'>More Filters</Button>
+        </div>
+    </div>
   )
 }
 
