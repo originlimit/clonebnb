61fdfa30f3a22b4bafdb4ca9b1d0d04b2f667c71
originlimit <andres-log@protonmail.com>
2022-04-23 23:59:54 -0700

    skeleton

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './SearchResults.css'
 
-const SearchResults = ({img, title, location, desc, star, price, total}) => {
+const SearchResults = ({ img, title, location, desc, star, price, total }) => {
   return (
-    <div className='searchResults'>SearchResults</div>
+    <div className='searchResults'>
+        <div className='searchResults__img'>image</div>
+        <div className='searchResults__info'>
+            <div className='searchResults__info__top'>
+                title, loc, heart
+            </div>
+            <div className='searchResults__info__mid'>
+                desc
+            </div>
+        </div>
+    </div>
   )
 }
 
