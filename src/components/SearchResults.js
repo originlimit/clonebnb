07a966ae67bcb1f5d1d8ba469c7e94f3e4a884ca
originlimit <andres-log@protonmail.com>
2022-04-28 12:01:42 -0700

    added temporary dummy text

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import './SearchResults.css'
-import StarIcon from '@mui/icons-material/Star'
+import StarRoundedIcon from '@mui/icons-material/StarRounded'
+import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
 
 const SearchResults = ({ img, title, location, desc, star, price, total }) => {
   return (
     <div className='searchResults'>
-        <div className='searchResults__img'>image</div>
+        <img src={ img } alt='property view' />
         <div className='searchResults__info'>
-            <div className='searchResults__info__top'>
-                {title} {location}
+            <div className='searchResults__infoTop'>
+                { title } { location } <FavoriteBorderRoundedIcon />
             </div>
-            <div className='searchResults__info__mid'>
-                <hr></hr>
-                {desc}
-            </div>
-            <div className='searchResults__info__btm'>
-                {price} <StarIcon /> {star} {total}
+            <hr></hr>
+                { desc }
+            <div className='searchResults__infoBtm'>
+                { price } <StarRoundedIcon />{ star } { total }
             </div>
         </div>
     </div>
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -14,7 +14,7 @@ const SearchPage = () => {
             <Button variant='outlined'>Price</Button>
             <Button variant='outlined'>Rooms and Beds</Button>
             <Button variant='outlined'>More Filters</Button>
-            <SearchResults />
+            <SearchResults title='This is a title' location='60th St. Los Angeles' desc='This is a description about a small apartment in LA' price='$30 per night' star='4.5' total='$120 total' />
         </div>
     </div>
   )
