aeffdf82f8acade2d984d88e83a0c1b54d6aa1e9
originlimit <andres-log@protonmail.com>
2022-04-30 10:17:09 -0700

    added links to explore nearby button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Hero.css'
 
 const Hero = () => {
@@ -8,7 +9,9 @@ const Hero = () => {
         <div className='hero__info'>
             <h1>Get out and experience life!</h1>
             <h4>Plan a stay at a nice place and witness beauty while staying comfortable.</h4>
-            <Button>Explore Nearby</Button>
+            <Link className='hero__infoLink' to='/search'>
+                <Button>Explore Nearby</Button>
+            </Link>
         </div>
     </div>
   )
