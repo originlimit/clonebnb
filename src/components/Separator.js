9d39193f3947b3b89545be3bb751dfe879f92039
originlimit <andres-log@protonmail.com>
2022-04-30 08:10:30 -0700

    save point for change in card style

--- src/components/Separator.js
+++ src/components/Separator.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Separator.css'
 
+// reuse in terms page and searchpage
+
 const Separator = () => {
   return (
     <>
-      <hr></hr>
+      <hr className='sep'></hr>
     </>
   )
 }
