edae97ec09fb1a79757da3b261d4a57715aaa56e
originlimit <andres-log@protonmail.com>
2022-05-07 20:29:19 -0700

    made card components mobile friendly

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({src, title, desc, price}) => {
         <div className='card'>
             <img className='card__img' src={src} alt='card visual' />
             <div className='card__info'>
-                <h2 className='card__content'>{title}</h2>
+                <h2 className='card__content card__title'>{title}</h2>
                 <h4 className='card__content'>{desc}</h4>
                 <h4 className='card__content card__price'>{price}</h4>
             </div>
